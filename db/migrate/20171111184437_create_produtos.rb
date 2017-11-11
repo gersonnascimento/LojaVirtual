a803d78b7a03e91d0633d7ca53901b3400034726
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-11-11 00:13:29 -0200

    teste2

--- db/migrate/20171111184437_create_produtos.rb
+++ db/migrate/20171111184437_create_produtos.rb
@@ -1,6 +1,6 @@
 class CreateProdutos < ActiveRecord::Migration[5.0]
   def change
-    create_table :usuarios do |t|
+    create_table :produtos do |t|
       t.string :nome
       t.string :descricao
       t.integer :quantidade
